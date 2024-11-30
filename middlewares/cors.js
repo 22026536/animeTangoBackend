cb3092307f70b70b0f2f6d9c23615599fd38c95f
22026536 <22026536>
2024-11-30 09:27:03 +0700

    quang

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,13 +4,14 @@ const whitelist = new Set(['http://example.com', 'https://example.org']);
 
 const corOptions = {
     optionsSuccesStatus: 200,
-    origin: function(origin,callback) {
-        if(whitelist.has(origin)) {
-            callback(null,true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    // origin: function(origin,callback) {
+    //     if(whitelist.has(origin)) {
+    //         callback(null,true);
+    //     } else {
+    //         callback(new Error("Not allowed by CORS"));
+    //     }
+    // },
+    origin : "*",
     credentials: true
 }
 
