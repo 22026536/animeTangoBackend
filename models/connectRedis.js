f266da1f36ded70b7789c3d6a05f69f36fa65b60
22026536 <22026536>
2024-11-30 10:47:30 +0700

    quang

--- models/connectRedis.js
+++ models/connectRedis.js
@@ -3,8 +3,7 @@ import { createClient } from 'redis';
 dotenv.config();
 
 const redisClient= createClient({
-    host: 'redis-12227.c295.ap-southeast-1-1.ec2.redns.redis-cloud.com', // Tên máy chủ Redis
-    port: 6379, // Cổng máy chủ Redis
+    url: process.env.REDIS_URL,
 });
 
 export default redisClient
