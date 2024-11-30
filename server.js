98c80038b64c6ca24549a86c8ab0c93b2d812764
22026536 <22026536>
2024-11-30 10:43:26 +0700

    quang

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express, { json } from 'express';
-import session from 'express-session';
 import mongoose from 'mongoose';
 import Router from './api/user/index.js';
 import corMw from "./middlewares/cors.js";
+import redisClient from "./models/connectRedis.js";
+
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }  // Bỏ secure nếu không dùng HTTPS
-}));
+
+await redisClient.connect()
 app.options('*', corMw);
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(json());
-app.set('trust proxy', 1)
+app.set('trust proxy', 1);
 app.use(cookieParser());
 app.use('/api', Router);
 
-mongoose.connect("mongodb+srv://sangvo22026526:5anG15122003@cluster0.rcd65hj.mongodb.net/anime_tango2"),{
+// Kết nối MongoDB
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverSelectionTimeoutMS: 30000, // Tăng thời gian chờ lên 30 giây
-};
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
