40dae211b1469d167b895cbe51ef3fae058ff1b0
22026536 <22026536>
2024-11-30 11:05:09 +0700

    quang

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import cookieParser from 'cookie-parser';
+import cors from 'cors'; // Import thư viện cors
 import dotenv from 'dotenv';
 import express, { json } from 'express';
 import mongoose from 'mongoose';
 import Router from './api/user/index.js';
+
 
 dotenv.config();
 
