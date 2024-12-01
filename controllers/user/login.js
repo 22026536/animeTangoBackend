75b21b925fdb023d72c65c41794ae847dc78c289
22026536 <22026536>
2024-12-01 17:59:15 +0700

    'quang'

--- controllers/user/login.js
+++ controllers/user/login.js
@@ -46,11 +46,10 @@ const login = async (req, res) => {
         // Step 5: Set JWT cookie and respond with success
         res.cookie("jwt", token, {
             httpOnly: true, // Cookie chỉ được truy cập qua HTTP, không phải JavaScript
-            secure: true,   // Chỉ gửi cookie qua HTTPS (bắt buộc khi chạy trên môi trường production)
+            secure: false,   // Chỉ gửi cookie qua HTTPS (bắt buộc khi chạy trên môi trường production)
             sameSite: 'Lax', // Ngăn chặn cookie bị gửi qua các request cross-site (tùy trường hợp bạn có thể dùng 'Strict' hoặc 'None')
             maxAge: 1000 * 60 * 30 })// Cookie tồn tại 30 phút maxAge: 1000 * 60 * 30 });
         //.cookie("name", user.full_name, { maxAge: 1000 * 60 * 30 }).cookie("birthday", user.date_of_birth, { maxAge: 1000 * 60 * 30 }).cookie("phoneNumber", user.phone_number, { maxAge: 1000 * 60 * 30 }).cookie("address", user.address, { maxAge: 1000 * 60 * 30 });
-        console.log(req)
         if (user.role === 0) {
             return res.json({
                 message: "user",
