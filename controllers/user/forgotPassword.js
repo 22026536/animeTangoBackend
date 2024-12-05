bbb6f654b0935f82d9567fed4b33ded1032f533d
22026536 <22026536>
2024-12-05 23:22:24 +0700

    'quang'

--- controllers/user/forgotPassword.js
+++ controllers/user/forgotPassword.js
@@ -29,19 +29,14 @@ export const forgotPassword = async (req, res) => {
 
         // Send verification email
         const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            service: 'gmail',
-            auth: {
-                type: "OAUTH2",
-                user: process.env.MY_EMAIL,
-                clientId: "vfo9u2o435uk2jjfvlfdkpg284u3.apps.googleusercontent.com",
-                clientSecret: "og029503irgier0oifwori",
-                refreshToken: "2093402i3jflj;geijgp039485puihsg[-9a[3;wjenjk,ucv[3485p0o485uyr;ifasjsdo283wefwf345w]fw2984329oshfsh",
-                accessToken: "owiejfw84u92873598yiuhvsldiis9er0235983isudhfdosudv3k798qlk3j4094too283982fs",
-                expires: 3599
-            }
+            host: process.env.MAIL_HOST,
+            port: process.env.MAIL_PORT,
+            secure: false,
+            // service: 'gmail',
+            // auth: {
+            //     user: process.env.MY_EMAIL,
+            //     pass: process.env.MY_EMAIL_PASSWORD
+            // }
         });
 
         const mailOptions = {
@@ -94,7 +89,7 @@ export const forgotPasswordCheck = async (req, res) => {
                 success: false
             });
         }
-
+        
         return res.json({
             message: "Mã xác thực chính xác",
             success: true
