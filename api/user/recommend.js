2cce61f25e1a1da3235159640181fccff00a7154
22026536 <22026536>
2024-12-10 18:44:37 +0700

    'quang'

--- api/user/recommend.js
+++ api/user/recommend.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { recommendDecisionTreeUser } from "../../controllers/user/recommend.js";
+import { recommendDecisionTreeUser, recommendNaiveBayesUser } from "../../controllers/user/recommend.js";
 
 const routerRecommend = express.Router()
 routerRecommend.post("/user/decisiontree",recommendDecisionTreeUser)
-
+routerRecommend.post("/user/naivebayes",recommendNaiveBayesUser)
 export default routerRecommend;
