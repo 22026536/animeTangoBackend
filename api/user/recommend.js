4560d6aac9b65f3d34369054cea0931ea22e9215
22026536 <22026536>
2024-12-10 20:30:13 +0700

    'quang'

--- api/user/recommend.js
+++ api/user/recommend.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { recommendDecisionTreeUser, recommendNaiveBayesUser } from "../../controllers/user/recommend.js";
+import { recommendDecisionTreeUser, recommendKNNUser, recommendNaiveBayesUser } from "../../controllers/user/recommend.js";
 
 const routerRecommend = express.Router()
 routerRecommend.post("/user/decisiontree",recommendDecisionTreeUser)
 routerRecommend.post("/user/naivebayes",recommendNaiveBayesUser)
+routerRecommend.post("/user/knn",recommendKNNUser)
 export default routerRecommend;
--- controllers/user/recommend.js
+++ controllers/user/recommend.js
@@ -78,3 +78,43 @@ export const recommendNaiveBayesUser = async (req, res) => {
         return res.status(500).json({ message: "Internal server error", error: error.message });
     }
 };
+
+
+export const recommendKNNUser = async (req, res) => {
+    try {
+        // Lấy token từ body
+        const token = req.body.jwt;
+        if (!token) {
+            return res.json({
+                message: "Người dùng chưa đăng nhập",
+                success: false
+            });
+        }
+
+        // Kiểm tra xem token có hết hạn không
+        if (isTokenExpired(token)) {
+            return res.json({
+                message: "Người dùng hết phiên đăng nhập",
+                success: false
+            });
+        }
+
+        // Giải mã token để lấy thông tin người dùng
+        const decoded = verifyToken(token);
+        const user_id = decoded.id;
+        const n = req.body.n || 10
+
+        // Gửi yêu cầu POST đến API bên ngoài để nhận thông tin người dùng
+        const response = await axios.post('https://animetangorecommendknn2.onrender.com/', { user_id: user_id, n: n });
+
+            return res.json({
+                message: "Lấy thông tin người dùng thành công",
+                success: true,
+                data: response.data
+            });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
